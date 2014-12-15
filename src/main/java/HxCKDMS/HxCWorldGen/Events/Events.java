ba01490492b7ca324d69f60b0c1a7b3cb365af08
DrZed <kyahne12@yahoo.com>
2014-12-15 04:14:58 -0600

    Disabled anoying Tooltip xD sorry clown

--- src/main/java/HxCKDMS/HxCWorldGen/Events/Events.java
+++ src/main/java/HxCKDMS/HxCWorldGen/Events/Events.java
@@ -1,17 +1,12 @@
 package HxCKDMS.HxCWorldGen.Events;
 
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
-import cpw.mods.fml.relauncher.Side;
-import cpw.mods.fml.relauncher.SideOnly;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
-
 public class Events {
-    @SubscribeEvent
-    @SideOnly(Side.CLIENT)
-    public void ItemTooltipEvent(ItemTooltipEvent event){
-        //keep this in here Plz ~DrZed
-        if (event.entityPlayer.getDisplayName().equalsIgnoreCase("UnethicalClown")){
-            event.toolTip.add("\u00A74This is dedicated to Carla");
-        }
-    }
+//    @SubscribeEvent
+//    @SideOnly(Side.CLIENT)
+//    public void ItemTooltipEvent(ItemTooltipEvent event){
+//        keep this in here Plz ~DrZed
+//        if (event.entityPlayer.getDisplayName().equalsIgnoreCase("UnethicalClown")){
+//            event.toolTip.add("\u00A74This is dedicated to Carla");
+//        }
+//    }
 }
