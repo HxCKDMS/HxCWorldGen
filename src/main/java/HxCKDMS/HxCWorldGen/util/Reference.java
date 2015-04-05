85a98f47a5b57495b2d3f11b1dd4c40f434dcebc
karelmikie3 <karelkling1@gmail.com>
2015-04-05 03:12:58 +0200

    update

--- src/main/java/HxCKDMS/HxCWorldGen/util/Reference.java
+++ src/main/java/HxCKDMS/HxCWorldGen/util/Reference.java
@@ -3,6 +3,6 @@ package HxCKDMS.HxCWorldGen.util;
 public class Reference {
     public static final String MOD_ID = "HxCWorldGen";
     public static final String MOD_NAME = "HxC World Gen";
-    public static final String VERSION = "1.7.10-1.0a";
+    public static final String VERSION = "1.7.10-1.0b";
     public static final String DEPENDENCIES = "required-after:HxCCore";
 }
