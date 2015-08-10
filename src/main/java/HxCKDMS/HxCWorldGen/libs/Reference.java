b1e4a795c5733119304fbe60e7089ceb7d3a19eb
DrZed <DrZed@users.noreply.github.com>
2015-08-10 14:48:15 -0400

    Update Reference.java

--- src/main/java/HxCKDMS/HxCWorldGen/libs/Reference.java
+++ src/main/java/HxCKDMS/HxCWorldGen/libs/Reference.java
@@ -3,8 +3,8 @@ package HxCKDMS.HxCWorldGen.libs;
 public class Reference {
     public static final String MOD_ID = "HxCWorldGen";
     public static final String MOD_NAME = "HxC World Gen";
-    public static final String VERSION = "1.7.10-1.4.0";
-    public static final String DEPENDENCIES = "required-after:HxCCore@[1.5.0,)";
+    public static final String VERSION = "1.7.10-1.4.1";
+    public static final String DEPENDENCIES = "required-after:HxCCore@[1.8.1,)";
     public static final String CLIENT_PROXY_CLASS = "HxCKDMS.HxCWorldGen.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "HxCKDMS.HxCWorldGen.proxy.ServerProxy";
     public static final String RESOURCE_LOCATION = "hxcworldgen:";
