4f52c8fcabff3fc748a6bf9989f9739b37a42ffd
DrZed <kyahne14@gmail.com>
2015-08-12 22:47:06 -0400

    bumped core version

--- src/main/java/HxCKDMS/HxCWorldGen/libs/Reference.java
+++ src/main/java/HxCKDMS/HxCWorldGen/libs/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
     public static final String MOD_ID = "HxCWorldGen";
     public static final String RESOURCE_LOCATION = MOD_ID.toLowerCase() + ":";
     public static final String VERSION = "1.5.0";
-    public static final String DEPENDENCIES = "required-after:HxCCore@[1.8.0,)";
+    public static final String DEPENDENCIES = "required-after:HxCCore@[1.8.1,)";
     public static final String CLIENT_PROXY_CLASS = "HxCKDMS.HxCWorldGen.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "HxCKDMS.HxCWorldGen.proxy.ServerProxy";
 
